0cad3611ed84b26d630daca24e68b33e64e1ad6a
Ilya Obukhov <MustD@users.noreply.github.com>
2024-07-14 21:57:44 +0200

    6 fix gql code generation requires auth (#8)
    * Update authentication settings in GQL routes
    
    Made the authentication optional within gqlRoutes in GQL.kt file to make the GraphQL routes more accessible. Also, the graphQLSubscriptionsRoute is relocated inside the authentication block for securing the subscriptions.
    
    Signed-off-by: md <ilya.obukhov.bs@gmail.com>
    
    * Add authentication to GraphQL server and refactor code format
    
    Implemented JWT authentication in GraphQL server context, enabling retrieval of JWT principal in data fetching environment. This allows for future context-based authorization. Additionally, made some adjustments to code formatting for better readability and organized import statements.
    
    Signed-off-by: md <ilya.obukhov.bs@gmail.com>
    
    ---------
    
    Signed-off-by: md <ilya.obukhov.bs@gmail.com>

--- bp_back/src/main/kotlin/com/bagplease/plugins/GQL.kt
+++ bp_back/src/main/kotlin/com/bagplease/plugins/GQL.kt
@@ -3,10 +3,14 @@
 package com.bagplease.plugins
 
 import com.bagplease.gql.GqlDefinition
+import com.expediagroup.graphql.generator.extensions.plus
 import com.expediagroup.graphql.server.ktor.*
+import graphql.GraphQLContext
 import io.ktor.serialization.jackson.*
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
+import io.ktor.server.auth.jwt.*
+import io.ktor.server.request.*
 import io.ktor.server.routing.*
 import io.ktor.server.websocket.*
 import java.time.Duration
@@ -19,6 +23,9 @@ fun Application.configureGql() {
             queries = GqlDefinition.queries
             mutations = GqlDefinition.mutations
             subscriptions = GqlDefinition.subscriptions
+        }
+        server {
+            contextFactory = CustomGraphQLContextFactory()
         }
     }
 
@@ -36,4 +43,23 @@ fun Routing.gqlRoutes() {
         graphiQLRoute()
     }
     graphQLSubscriptionsRoute()
+
+}
+
+const val GQL_CALL_PRINCIPAL = "callPrincipal"
+
+class CustomGraphQLContextFactory : DefaultKtorGraphQLContextFactory() {
+    /*
+        // now we can use context auth  like below
+        suspend fun getItems(env: DataFetchingEnvironment): List<GqlItem> {
+            val principal = env.graphQlContext.get<JWTPrincipal>(GQL_CALL_PRINCIPAL)
+            return service.getItems().map(GqlItemMapper::mapItemToGql)
+        }
+
+     */
+
+    override suspend fun generateContext(request: ApplicationRequest): GraphQLContext =
+        super.generateContext(request).plus(
+            mapOf(GQL_CALL_PRINCIPAL to request.call.principal<JWTPrincipal>())
+        )
 }
